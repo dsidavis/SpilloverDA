0c2e87efd5e2a93310dfddc6865e6ab08a1e60f6
Duncan Temple Lang <duncan@r-project.org>
2018-01-22 14:30:35 -0800

    change the test to avoid the intermediate rds file

--- tests/nonASCII.R
+++ tests/nonASCII.R
@@ -1,17 +1,21 @@
 library(SpilloverDA)
-if(FALSE){
+if(FALSE)
+{
 # Assumes you are sitting in Zoonotic-shared
     f = "LatestDocs/PDF/0013939837/Huang-2012-Simian foamy virus prevalence in Ma.xml"
 
-    tmp = xml2eco(f, cache.dir = NA, results_dir = NA)
-    tmp_file = tempfile()
-    saveRDS(tmp, tmp_file)
+    if(file.exists('bob.rds'))
+       tmp = readRDS('bob.rds')
+    else
+       tmp = xml2eco(f, cache.dir = NA, results_dir = NA)
+
+    loc = getLocation(tmp)        
     # Gets killed by non-ASCII char
-    tmp2 = mkCSV(tmp_file)
+    tmp2 = mkCSV(eco = loc, write = TRUE, xlsxFile = "test.xlsx")
 
     # this corrects it
-    tmp3 = mkCSV(tmp_file, write = FALSE)
-    tmp3 = sapply(tmp3, stringi::stri_trans_general, id = "latin-ascii")
-    openxlsx::write.xlsx(tmp3, tempfile())
+#    tmp3 = mkCSV(tmp_file, write = FALSE)
+#    tmp3 = sapply(tmp3, stringi::stri_trans_general, id = "latin-ascii")
+#    openxlsx::write.xlsx(tmp3, tempfile())
 }
     
