aeabe6651d20ac9b80423e5e20ce6b9dc394e90f
Matt Espe <mespe@ucdavis.edu>
2018-04-10 14:31:09 -0700

    Adds simple test for char string.

--- tests/test_ecoextract.R
+++ tests/test_ecoextract.R
@@ -6,9 +6,14 @@ library(SpilloverDA)
 sample_text = list(title = "My PCR experiment in China",
                    abstract = "This is just some text which will be used as filler. There is a diagnostic test nested-PCR mentioned here, as well as a species Rattus rattus. There is no mention of country names or the virus. If I add antibodies, I should also recover this.")
 
-ans = doc2eco(section.text = sample_text)
+ans = doc2keywords(section.text = sample_text)
 
 getDiagTest(ans)
 getCountry(ans)
 getVirus(ans)
 getSpecies(ans)
+
+# try on just a single char vector
+short_text = "This mentions China."
+
+doc2keywords(section.text = short_text)
