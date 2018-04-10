49f0a827054a2c1ebe34502d3dfddf735d953e76
Matt Espe <mespe@ucdavis.edu>
2018-04-10 10:48:44 -0700

    Moved over from Zoonotics-shared/R/pdf2eco.R.

--- R/pdf2eco.R
+++ R/pdf2eco.R
@@ -1,53 +1,59 @@
 # Function to go from a PDF file to JSON
 
-
-xml2eco =
-    # Handle a PDF file as well.
-function(XML, ecoextract = getEcoExtractPyScript(),
-         results_dir = character(), cache.dir = character())
+xml2eco = function(XML, ecoextract = "EcoHealth/ecoextract.py",
+                   results_dir = "ecoJSON", cache.dir = "sections",
+                   cache.file = file.path(cache.dir, gsub("xml$", "rds", basename(XML))),
+                   section_text = getXMLText(cache.file),
+                   save = FALSE, save_file = file.path(results_dir, gsub("xml$", "rds", basename(XML))))
 {
-    if(length(cache.dir) && !is.na(cache.dir))  {
-       if(!dir.exists(cache.dir))
-            dir.create(cache.dir)
-
-       cache.file = file.path(cache.dir, gsub("xml$", "rds", basename(XML)))
-       if(exists(cache.file))
-           ll = readRDS(cache.file)
-       else {
-           ll = readXMLSections(XML)
-           saveRDS(ll, file = cache.file)
-       }
-     } else {
-           ll = readXMLSections(XML) # repeated from above. Fix!
-     }
-       
-    
     # Clean up special characters
-    ll = lapply(ll, function(x) gsub('Â|"', "", x))
+    section_text = lapply(section_text, function(x) gsub('Â|"', "", x))
 
     # Drop references, etc.
-    i = grep("references?|cited|acknowledgements?|funding|interests|disclosure", names(ll),
+    i = grep("references?|cited|acknowledgements?|funding|interests|disclosure", names(section_text),
              ignore.case = TRUE, invert = TRUE)
-    ll = ll[i]
-    ans = lapply(ll, function(x) try(sect2eco(x, ecoextract)))
+    section_text = section_text[i]
+    ans = lapply(section_text, function(x) try(sect2eco(x, ecoextract = ecoextract)))
 
-    if(length(results_dir) && !is.na(results_dir)) {
-        if(!dir.exists(results_dir))
-            dir.create(results_dir)
-        
-        saveRDS(ans, file.path(results_dir, gsub("xml$", "rds", basename(XML))))
-    }
+    if(save)
+        saveRDS(ans, save_file)
     ans
 }
 
-sect2eco = function(section, ecoextract = getEcoExtractPyScript())
+getXMLText = function(cache.file)
+{
+    if(!exists(cache.file)){
+        doc = readPDFXML(XML)
+        f = tempfile()
+        
+        title = getDocTitleString(doc)
+        abst = findAbstract(doc)
+        
+        abst = paste(names(abst), collapse = " ")
+        
+        allsect = try(lapply(getSectionText(doc), function(x)
+            paste(unlist(x), collapse = " ")))
+        
+        # browser()
+    
+        if(is(allsect, "try-error") | length(allsect) > 20)
+            allsect = list(body = getDocText(doc))
+        
+        ll = c(title = title, abstract = abst, allsect)
+        saveRDS(ll, file = cache.file)
+    } else {
+        ll = readRDS(cache.file)
+    }
+    return(ll)
+}
+
+sect2eco = function(section, ecoextract = "EcoHealth/ecoextract.py")
 {
     f = tempfile()
     f2 = tempfile()
 
     cat(section, file = f)
-    #    system2(ecoextract, args = c(f, f2))
-    system2("python3", args = c(ecoextract, f, f2))
+    system2(ecoextract, args = c(f, f2))
     res = fromJSON(f2)
     res$txt = section
     res
@@ -75,49 +81,34 @@ fixSectionNames = function(sect_names)
 }
 
 
-readXMLSections =
-function(XML)
-{    
-    doc = readPDFXML(XML)
-
-    title = getDocTitleString(doc)
-    abst = findAbstract(doc)
-        
-    abst = paste(names(abst), collapse = " ")
-        
-    allsect = try(lapply(getSectionText(doc), function(x)
-                                                  paste(unlist(x), collapse = " ")))
-       
-    if(is(allsect, "try-error") || length(allsect) > 20)
-        allsect = list(body = getDocText(doc))
-        
-    c(title = title, abstract = abst, allsect)
-}
-
-
-
-getEcoExtractPyScript =
-function()
+expand_abbrevs = function(txt)
 {
-  getOption("EcoExtractScript", system.file("python", "ecoextract.py", package = "SpilloverDA"))
-}
+    # txt = "This is a Genus species example of an abbreviation G. species, but this is M. Espe's name."
+    txt = strsplit(txt, " +")[[1]]
+    pos = grep("[A-Z]\\.", txt)
+    next_word = txt[pos + 1]
+    next_word = gsub("\\(|\\)|,|;|\\.", "", next_word)
+    other_pos = lapply(next_word, function(x) {
+        possible_pos = grep(x, txt)
+        setdiff(possible_pos, pos + 1)
+    })
+    
+    replace_term = lapply(seq(along = other_pos), function(i){
+        x = txt[other_pos[[i]] - 1]
+        ans = txt[pos[[i]]]
+        if(length(x) != 0){
+            if(substr(ans, 1, 1) == substr(x, 1, 1))
+                ans = x
+        }
+        x             
+    })
 
+    mod_txt = txt
+    for(i in seq(along = pos)){
+        if(length(replace_term[[i]]) != 0){
+            mod_txt[pos[i]] = replace_term[[i]]
+        }
+    }
 
-getEpitatorDBFilename =
-function()
-{
-    val = Sys.getenv("ANNOTATOR_DB_PATH")
-    if(is.na(val) || val == "")
-        val = path.expand("~/.epitator.sqlitedb")
-    val
-}
-
-checkEpitatorDB =
-function(db = getEpitatorDBFilename(), error = TRUE)
-{
-    status = file.exists(db)
-    if(error && !status)
-        stop("Epitator SQLite database doesn't exist: ", db)
-
-    status
+    return(paste(mod_txt, collapse = " "))
 }
