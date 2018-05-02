c3360df790b589b5ce19c195a0becbc03322223f
Matt Espe <mespe@ucdavis.edu>
2018-05-02 12:54:07 -0600

    Add functions to make the JSON required by the HTML page.

--- R/classify.R
+++ R/classify.R
@@ -22,7 +22,7 @@ collapseResults = function(testSet)
         data.frame(var = unique(x$var),
                    sections = paste(unique(x$sectionName), collapse = ";"),
                    pdf = unique(x$pdf),
-                   avg_score = mean(x$score),
+                   avg_score = round(mean(x$score, na.rm = TRUE), 3),
                    stringsAsFactors = FALSE)
     })
     ans = do.call(rbind, ans)
@@ -30,10 +30,54 @@ collapseResults = function(testSet)
     ans
 }
 
-resultsByPDF = function(resultsList)
+mkJSON = function(resultsList, pdfNames, saveDir = character())
 {
-    
+    ans = resultsByPDF(resultsList, pdfNames, saveDir)
+    ans
 }
+
+
+resultsByPDF = function(resultsList, pdfNames, saveDir = character(),
+                        outfiles = file.path(saveDir,
+                                             gsub("\\.pdf$|\\.xml$", "_extract.json",
+                                                  basename(pdfNames))))
+{
+    ans = lapply(seq(along = pdfNames), function(i){
+        ans = getExtractByPDF(resultsList, pdfNames[i])
+        ans = lapply(ans, function(x) x[order(x$avg_score, decreasing = TRUE),])
+        if( length(saveDir) != 0 ){
+            if(!dir.exists(saveDir))
+                dir.create(saveDir)
+            saveResultsJSON(ans, outfiles[i])
+        }
+    })
+    ans
+}
+
+getMetaInfo = function(extractResults)
+{
+    lapply(seq(along = extractResults), function() {
+        data.frame(title = extractResults[[i]]$title$txt,
+                   abstract = extractResults[[i]]$abstract$txt,
+                   pdf = names(extractResults)[i],
+                   year = integer(),
+                   stringsAsFactors = FALSE)
+        })
+}
+    
+getExtractByPDF = function(resultsList, pdfName)
+{
+    lapply(resultsList, function(x) {
+        x[x$pdf == pdfName,]
+    })
+}
+
+saveResultsJSON = function(ans, outfile)
+{
+    write(toJSON(ans), file = outfile)
+    return(outfile)
+}
+    
 ################################################################################
 # Plotting code - maybe move elsewhere?
 
