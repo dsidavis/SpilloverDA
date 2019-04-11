17edbc71427e3e05ebd243bb6f4c7d4d14cd5f2c
Matt Espe <mespe@ucdavis.edu>
2019-04-10 20:34:54 -0600

    Fix Image convert functions.

--- R/scannedUtils.R
+++ R/scannedUtils.R
@@ -6,17 +6,19 @@ getPDFconvertUtil = function()
 PDFtoImage = function(pdf_file, out_file = character(),
                       convertUtil = getPDFconvertUtil(),
                       out_format = "png",
-                      args = c("-density 300", "-background white", "-alpha remove"))
+                      args = c("-density 300", "-background white", "-alpha remove", "-verbose"))
 {
     if(length(out_file) == 0 & grepl("\\.pdf$", pdf_file))
-        out_file = gsub("\\.pdf$". out_format, pdf_file)
+        out_file = gsub("\\.pdf$", paste0(".", out_format), pdf_file)
     
     cmd = sprintf("%s '%s' %s '%s'", convertUtil, path.expand(pdf_file),
                   paste(args, collapse = " "), path.expand(out_file))
-    system(cmd)
-    list.files(dirname(path.expand(out_file)),
-               pattern = gsub(paste0("\\.", out_format, "$"), "", out_file),
-               full.names = TRUE)
+    out = system(cmd, intern = TRUE)
+    list.files(path = dirname(out_file),
+               pattern = gsub(paste0("(\\.)", out_format, "$"),
+                              paste0(".*\\\\.", out_format),
+                              basename(out_file)),
+               full.name = TRUE)
 }
 
 ocrPDF = function(pdf_file,
